41d3be7242063fdd49b190816cde9d80f474dae6
Frida Mjaria <frida.mjaria@prodigyfinance.com>
2020-06-28 21:32:24 +0200

    Add authors

--- src/main/java/com/spellchecker/api/SpellcheckerApplication.java
+++ src/main/java/com/spellchecker/api/SpellcheckerApplication.java
@@ -3,11 +3,16 @@ package com.spellchecker.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author Nthabi Mashiane
+ *
+ */
+
 @SpringBootApplication
 public class SpellcheckerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpellcheckerApplication.class, args);
 	}
-
 }
--- src/main/java/com/spellchecker/api/SpellcheckerController.java
+++ src/main/java/com/spellchecker/api/SpellcheckerController.java
@@ -4,6 +4,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * @author Nthabi Mashiane
+ * @author frdamjaria
+ *
+ */
+
 @RestController
 public class SpellcheckerController {
 
