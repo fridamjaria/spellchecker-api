072f32639b1439b3e8ec60dd0e4d024d47ea203e
Frida Mjaria <frida.mjaria@prodigyfinance.com>
2020-06-28 21:33:13 +0200

    Add missing and refactor spellchecker functions

--- src/main/java/com/spellchecker/api/SpellcheckerFunctions.java
+++ src/main/java/com/spellchecker/api/SpellcheckerFunctions.java
@@ -8,22 +8,39 @@ import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SpellcheckerFunctions {
+/**
+ *
+ * @author frdamjaria
+ * @author Nthabi Mashiane
+ *
+ */
+
+public class SpellcheckerFunctions extends ErrorCorrector {
 
     public HashMap<String, Integer> trigramMap = new HashMap<>();
     private final HashSet<String> wordlist = new HashSet<>();
     public final HashSet<String> dictionary = new HashSet<>(); //Stores words added by the user
-    private String dictDatabas = ""; //to make writing back to file easier
+    private final StringBuilder dictDatabas = new StringBuilder(); //to make writing back to file easier
+    private final String language;
 
-    public SpellcheckerFunctions() {
+    public SpellcheckerFunctions(String language) {
+        this.language = language;
         initialize();
+        initCorrector(language);
     }
 
     /*
      * Checks word in wordlist before doing error detection
      * returns true if correct
      */
-    public boolean check(String str) {
+    public boolean check(String word) {
+        String str = word;
+
+        //Removes non-Alphabetic last char
+        if (!Character.isLetter(str.charAt(word.length()-1))) {
+            str = word.substring(0,word.length()-1 );
+        }
+
         if(str.length() < 3){
             return true;
         }
@@ -35,12 +52,13 @@ public class SpellcheckerFunctions {
             return errorDetection(str);
         }
     }
-
+    
     /*
      * Searches for a word from wordlist
      * returns true if word is in the wordlist
      */
-    public boolean search(String word) {
+    private boolean search(String word) {
+
         return wordlist.contains(word) || dictionary.contains(word);
     }
 
@@ -48,18 +66,22 @@ public class SpellcheckerFunctions {
      * Detects the errors of a word
      * returns: true if error was detected
      */
-    public boolean errorDetection(String word) {
+    /**ADD ERROR CORRECTION TO THIS METHOD*/
+    private boolean errorDetection(String word) {
         //Get trigrams of the word
         ArrayList<String> trigrams = wordTrigram(word);
         boolean error = false;
-        int fre = 0;
-        double probability, threshold = 0.003;
-        int totalWords = trigramMap.size();
+        double frequency, threshold = 700;//Frequency for isiXhosa
+
+        //Set frequency for isiZulu
+        if(language.equalsIgnoreCase("isizulu")){
+            threshold = 45;
+        }
 
         //calculate the probability of each trigram and check for correctness
         for (String trigram : trigrams) {
-            probability = (double) getFrequency(trigram) / (double) totalWords;
-            if (probability < threshold) {
+            frequency = getFrequency(trigram);
+            if (frequency < threshold) {
                 error = false; //the trigam is incorrect
                 break; //No neeed to continue iterations
             } else {
@@ -79,7 +101,7 @@ public class SpellcheckerFunctions {
         int pos = 0;
         while (pos < word.length()) {
             if ((pos + 3) >= word.length()) {
-                strTri.add(word.substring(pos, word.length()));
+                strTri.add(word.substring(pos));
                 pos = word.length();
             } else {
                 strTri.add(word.substring(pos, pos + 3));
@@ -94,24 +116,40 @@ public class SpellcheckerFunctions {
      */
     private void initialize() {
         try {
-            InputStream wordList = SpellcheckerFunctions.class.getResourceAsStream("resources/wordlist.txt");
-            InputStream tri = SpellcheckerFunctions.class.getResourceAsStream("resources/trigrams.txt");
+            InputStream wdlist;
+            InputStream tri;
+            File dict;
 
-            BufferedReader wdReader = new BufferedReader(new InputStreamReader(wordList));
+            if (language.equalsIgnoreCase("isixhosa")){
+                wdlist = SpellcheckerApplication.class.getResourceAsStream("resources/xhosaWordlist.txt");
+                tri = SpellcheckerApplication.class.getResourceAsStream("resources/xhosaTrigrams.txt");
+                dict = new File("user_xhosa_dictionary");
+
+            }else{
+
+                wdlist = SpellcheckerApplication.class.getResourceAsStream("resources/zuluWordlist.txt");
+                tri = SpellcheckerApplication.class.getResourceAsStream("resources/zuluTrigrams.txt");
+                dict = new File("user_zulu_dictionary");
+            }
+
+
+            BufferedReader wdReader = new BufferedReader(new InputStreamReader(wdlist));
             BufferedReader trigramReader = new BufferedReader(new InputStreamReader(tri));
 
-            File dict = new File("user_dictionary");
+
 
             // Load user dictionary if it exists
             String line;
             if (dict.exists()) {
                 BufferedReader dictReader = new BufferedReader(new FileReader(dict));
+
                 line = dictReader.readLine();
                 while (line != null) {
                     dictionary.add(line.trim());
-                    dictDatabas += line.trim();
+                    dictDatabas.append(line.trim());
                     line = dictReader.readLine();
                 }
+                dictReader.close();
             }
 
             //Load the wordlist
@@ -120,13 +158,14 @@ public class SpellcheckerFunctions {
                 wordlist.add(line.trim());
                 line = wdReader.readLine();
             }
-            wordList.close();
+            wdlist.close();
 
             //Load the trigram
             line = trigramReader.readLine();
             String[] entry;
             while (line != null) {
                 entry = line.split(" ");//trigram and frequency
+                //sumFrequencies += Integer.parseInt(entry[1]);
                 trigramMap.put(entry[0], Integer.parseInt(entry[1]));
                 line = trigramReader.readLine();
             }
@@ -144,57 +183,38 @@ public class SpellcheckerFunctions {
      * return: frequency of trigram
      */
     private int getFrequency(String trigram) {
-        if (trigramMap.containsKey(trigram)) {
-            return trigramMap.get(trigram);
-        } else {
-            return 0;
-        }
+        return trigramMap.getOrDefault(trigram, 0);
     }
 
     /*
      * Adds a new User word to user's personal dictionary
      */
     public void addWord(String word) {
-        FileWriter fw = null;
-        Writer bw = null;
+        BufferedWriter bw = null;
+        String file_name = language.equalsIgnoreCase("isixhosa") ? "user_xhosa_dictionary" : "user_zulu_dictionary";
+        File dict = new File(file_name);
 
         try {
-            //updates the content of the user dictionary*/
-            File dict = new File("user_dictionary");
-            if (!dict.exists()) {
-                dict.createNewFile();
-                fw = new FileWriter(dict);
-                bw = new BufferedWriter(fw);
+            bw = new BufferedWriter(new FileWriter(dict));
+            if (!dict.exists() && dict.createNewFile()) {
                 dictionary.add(word);
-                dictDatabas +=  word;
-                bw.write(dictDatabas);
+                dictDatabas.append(word);
+                bw.write(dictDatabas.toString());
             } else {
-                fw = new FileWriter(dict);
-                bw = new BufferedWriter(fw);
-                //updates the content of the user dictionary*/
                 dictionary.add(word);
-                dictDatabas += "\n" + word;
-                bw.write(dictDatabas);
+                dictDatabas.append("\n" + word);
+                bw.write(dictDatabas.toString());\
             }
-
-        } catch (IOException ex) {
-            Logger.getLogger(SpellcheckerFunctions.class.getName()).log(Level.SEVERE, null, ex);
-            JOptionPane.showMessageDialog(null, "File not found");
-        } finally {
+        } catch (IOException e) {
+            Logger.getLogger(SpellcheckerFunctions.class.getName()).log(Level.SEVERE, null, e);
+            JOptionPane.showMessageDialog(null, e);
+        }
+         finally {
             try {
                 bw.close();
-                fw.close();
             } catch (IOException ex) {
                 Logger.getLogger(SpellcheckerFunctions.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-
-//    private String ignorePunctuation(String word){
-//        Pattern pattern = Pattern.compile("\\p{Punct}");
-//        String newWord = word.replaceAll(",.?-","");
-//        newWord = newWord.toLowerCase();
-//        return newWord;
-//
-//    }
 }
