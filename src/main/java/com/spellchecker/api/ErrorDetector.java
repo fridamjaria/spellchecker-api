b276b7f8fc32d4b3d08ea994f44ba32068b04e42
Frida Mjaria <frida.mjaria@prodigyfinance.com>
2020-07-04 15:10:54 +0200

    Move ErrorDetection methods to separate class

---
+++ src/main/java/com/spellchecker/api/ErrorDetector.java
@@ -0,0 +1,111 @@
+package com.spellchecker.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ErrorDetector {
+    private HashSet<String> wordlist = new HashSet<>();
+    private HashMap<String, Integer> trigramMap;
+    private String language;
+
+    public ErrorDetector(String language, HashMap<String, Integer> trigramMap, HashSet<String> wordlist){
+        this.trigramMap = trigramMap;
+        this.language = language;
+        this.wordlist = wordlist;
+    }
+
+    public HashSet<String> detectErrors(String[] words){
+        HashSet<String> misspelledWords = new HashSet<>();
+
+        for(String word : words) {
+            word = stripPunctuation(word);
+
+            if(!(word.length() < 3 || search(word))){
+                misspelledWords.add(word);
+
+            }
+        };
+
+        return misspelledWords;
+    }
+
+    /*
+     * Gets the frequency of a trigram from a storage
+     * return: frequency of trigram
+     */
+    private int getFrequency(String trigram) {
+        return trigramMap.getOrDefault(trigram, 0);
+    }
+
+    /*
+     * Reads a word
+     * returns: word with any trailing punctuation stripped
+     */
+    private String stripPunctuation(String word){
+        if (!Character.isLetter(word.charAt(word.length()-1))) {
+            return word.substring(0,word.length()-1 );
+        }
+
+        return word;
+    }
+
+    /*
+     * Searches for a word from wordlist
+     * returns true if word is in the wordlist
+     */
+    private boolean search(String word) {
+
+        return wordlist.contains(word);
+    }
+
+    /*
+     * Detects the errors of a word
+     * returns: true if error was detected
+     */
+    private boolean isMispelled(String word) {
+        //Get trigrams of the word
+        ArrayList<String> trigrams = wordTrigram(word);
+        boolean error = false;
+        double frequency, threshold = 700;//Frequency for isiXhosa
+
+        //Set frequency for isiZulu
+        if(language.equalsIgnoreCase("isizulu")){
+            threshold = 45;
+        }
+
+        //calculate the probability of each trigram and check for correctness
+        for (String trigram : trigrams) {
+            frequency = getFrequency(trigram);
+            if (frequency < threshold) {
+                error = false; //the trigam is incorrect
+                break; //No neeed to continue iterations
+            } else {
+                error = true;
+            }
+        }
+
+        return error;
+    }
+
+    /*
+     * Reads a word word
+     * returns: a list of trigrams
+     */
+    private ArrayList<String> wordTrigram(String word) {
+
+        ArrayList<String> strTri = new ArrayList<>();
+        int pos = 0;
+        while (pos < word.length()) {
+            if ((pos + 3) >= word.length()) {
+                strTri.add(word.substring(pos));
+                pos = word.length();
+            } else {
+                strTri.add(word.substring(pos, pos + 3));
+                pos = pos + 1;
+            }
+        }
+        return strTri;
+    }
+
+}
--- src/main/java/com/spellchecker/api/SpellcheckerFunctions.java
+++ src/main/java/com/spellchecker/api/SpellcheckerFunctions.java
@@ -34,18 +34,9 @@ public class SpellcheckerFunctions extends ErrorCorrector {
      * returns HashMap of incorrect words and their respective correction suggestions
      */
     public HashMap<String, HashSet<String>> check(String[] text) {
-        HashSet<String> incorrect_words = new HashSet<>();
+        ErrorDetector detector = new ErrorDetector(language, trigramMap, wordlist)
+        HashSet<String> incorrect_words = detector.detectErrors(text);
         HashMap<String, HashSet<String>> corrections = new HashMap<>();
-
-        for(String word : text) {
-            word = stripPunctuation(word);
-
-            if(!(word.length() < 3 || search(word))){
-                if (errorDetection(word)){
-                    incorrect_words.add(word);
-                }
-            }
-        };
 
         incorrect_words.forEach(word -> {
             corrections.put(word, createSuggestions(word));
@@ -55,81 +46,11 @@ public class SpellcheckerFunctions extends ErrorCorrector {
     }
 
     /*
-     * Searches for a word from wordlist
-     * returns true if word is in the wordlist
-     */
-    private boolean search(String word) {
-
-        return wordlist.contains(word) || dictionary.contains(word);
-    }
-
-    /*
-     * Detects the errors of a word
-     * returns: true if error was detected
-     */
-    private boolean errorDetection(String word) {
-        //Get trigrams of the word
-        ArrayList<String> trigrams = wordTrigram(word);
-        boolean error = false;
-        double frequency, threshold = 700;//Frequency for isiXhosa
-
-        //Set frequency for isiZulu
-        if(language.equalsIgnoreCase("isizulu")){
-            threshold = 45;
-        }
-
-        //calculate the probability of each trigram and check for correctness
-        for (String trigram : trigrams) {
-            frequency = getFrequency(trigram);
-            if (frequency < threshold) {
-                error = false; //the trigam is incorrect
-                break; //No neeed to continue iterations
-            } else {
-                error = true;
-            }
-        }
-
-        return error;
-    }
-
-    /*
      * Reads in a word
      * returns: a set of suggestions for given word
      */
     private HashSet<String> createSuggestions(String word){
         return correct(word);
-    }
-
-    /*
-     * Reads a word word
-     * returns: a list of trigrams
-     */
-    private ArrayList<String> wordTrigram(String word) {
-
-        ArrayList<String> strTri = new ArrayList<>();
-        int pos = 0;
-        while (pos < word.length()) {
-            if ((pos + 3) >= word.length()) {
-                strTri.add(word.substring(pos));
-                pos = word.length();
-            } else {
-                strTri.add(word.substring(pos, pos + 3));
-                pos = pos + 1;
-            }
-        }
-        return strTri;
-    }
-
-    /*
-     * Reads a word
-     * returns: word with any trailing punctuation stripped
-     */
-    private String stripPunctuation(String word){
-        if (!Character.isLetter(word.charAt(word.length()-1))) {
-            return word.substring(0,word.length()-1 );
-        }
-
-        return word;
     }
 
     /*
@@ -194,14 +115,6 @@ public class SpellcheckerFunctions extends ErrorCorrector {
         } catch (IOException ex) {
             Logger.getLogger(SpellcheckerFunctions.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
-    /*
-     * Gets the frequency of a trigram from a storage
-     * return: frequency of trigram
-     */
-    private int getFrequency(String trigram) {
-        return trigramMap.getOrDefault(trigram, 0);
     }
 
     /*
